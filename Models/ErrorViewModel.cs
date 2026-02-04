a81746e520826ef38da43f932983644eedbf324e
kati kheirabadi <kati.kheirabadi@gmail.com>
2026-02-04 22:20:29 +0330

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoltanShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/navbar.js
@@ -0,0 +1,12 @@
+﻿document.querySelectorAll('.main-categories .cat-item').forEach(item => {
+    item.addEventListener('mouseenter', function () {
+
+        // remove active from all
+        document.querySelectorAll('.cat-item').forEach(i => i.classList.remove('active'));
+        document.querySelectorAll('.sub-box').forEach(i => i.classList.remove('active'));
+
+        // add active to current
+        this.classList.add('active');
+        document.getElementById(this.dataset.target).classList.add('active');
+    });
+});
